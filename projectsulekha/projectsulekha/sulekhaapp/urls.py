2cfc24036da25e1458b8a76be603cd7af5d8c31f
money gera <moneygera@Money-Gera.local>
2016-12-06 14:54:23 -0800

    updated with urls

--- projectsulekha/projectsulekha/sulekhaapp/urls.py
+++ projectsulekha/projectsulekha/sulekhaapp/urls.py
@@ -2,13 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     url(r'^signup/$', views.signup),
-   
-=======
     url(r'^home$/', views.home),
     url(r'^login$/', views.login),
     url(r'^register$/', views.register)  
     
->>>>>>> babar
 ]
