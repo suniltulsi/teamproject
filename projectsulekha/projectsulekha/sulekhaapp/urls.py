b9e8108319ece0aa60537e9ce2ec7e078d331fc0
babar007 <babarbaig@babars-MacBook-Pro.local>
2016-12-06 14:33:03 -0800

    Register page
    Corrected register page

--- projectsulekha/projectsulekha/sulekhaapp/urls.py
+++ projectsulekha/projectsulekha/sulekhaapp/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^home$/', views.home),
     url(r'^login$/', views.login),
-    url(r'^register$/', views.register),
-    
+    url(r'^register$/', views.register)  
     
 ]
